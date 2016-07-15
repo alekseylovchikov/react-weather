3649d15c62a2d4a557212ad391cccb7395f16a1e
Aleksey Lovchikov <aleksey.lovchikov@yandex.ru>
2016-07-15 09:33:15 +0000

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var path = require('path');
 var port = process.env.PORT || 3000;
 var host = process.env.IP || 'localhost';
 
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }    
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', function (req, res){
