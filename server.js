338283f75350e546d6849e110e0aef3435edd551
Aleksey Lovchikov <aleksey.lovchikov@yandex.ru>
2016-07-15 09:39:04 +0000

    fix npm

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('*', function (req, res){
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-})
+});
 
 app.listen(port, host, function(err) {
     if (err) {
